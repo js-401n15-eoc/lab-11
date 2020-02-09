ac2cfa4fdbc8a1895cc4d3a2762d184997f293c3
Earl Jay Caoile <unfie555@gmail.com>
2020-02-09 11:39:04 -0800

    made server.js and started users.js

--- auth/server.js
+++ auth/server.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const base64 = require('base-64');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(express.json());
+
+let secret = 'yourdoghasfleas';
 
 // users database
 let users = {};
@@ -18,14 +21,45 @@ app.post('/signup', async (req, res) => {
     user.password = await bcrypt.hash(req.body.password, 5);
     users[user.username] = user;
 
-    let token = 'blahblahblah';
+    let userObject = {
+      username: user.username,
+      isGreat: true,
+    };
+
+    let token = jwt.sign(userObject, secret);
+
     res.status(200).send(token);
   } else {
-    res.status(403).send();
+    res.status(403).send('username is already in use');
   }
 });
 
-app.post('/signin', (req, res) => {});
+// http post :3000/signin -a john:hasadog
+app.post('/signin', async (req, res) => {
+  // console.log(req.headers.authorization);
+  let basic = req.headers.authorization(' ').pop();
+  let [user, password] = base64.decode(basic).split(':');
+
+  let verified = user[user]
+    ? await bcrypt.compare(password, users[user].password)
+    : false;
+
+  if (verified) {
+    let userObject = {
+      username: user.username,
+      isGreat: true,
+    };
+
+    let token = jwt.sign(userObject, secret);
+
+    res.status(200).send(token);
+  } else {
+    res.status(403).send('Access Denied');
+  }
+  res.send('ok');
+});
+
+app.get('/secretStuff', auth, (req, res) => {});
 
 module.exports = {
   start: port =>
